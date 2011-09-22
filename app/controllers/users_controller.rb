d36c10cd0ccffcc0cee8146feebc9def370629e1
Mr Bubba <ttf@lavabit.com>
2011-09-22 22:13:41 +0100

    Take 2 on the users commit.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,4 +6,9 @@ class UsersController < ApplicationController
   def show
   	@user = User.find(params[:id])
   end
+  
+  def show
+  	@user = User.find(params[:id])
+  	@title = @user.name
+  end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -104,5 +104,22 @@ describe User do
 				@user.has_password?("invalid").should be_false
 			end
 		end
+		
+		describe "authenticate" method
+			it "shall return nil on email/password mismatch" do
+				wrong_password_user = User.authenticate(@attr[:email], "wrongpass")
+				wrong_password_user.should be_nil
+			end
+			
+			it "should return nil for an email address with no user" do
+				nonexistent_user = User.authenticate("bar@foo", @attr[:password])
+				nonexistent_user.should be_nil
+			end
+			
+			it "should return the user on email/password match" do
+				matching_user = User.authenticate(@attr[:email], @attr[:password])
+				matching_user.should == @user
+			end
+		end
 	end
 end
